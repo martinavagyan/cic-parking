8a6fc1cfe682573ac4358a52640edfd2d6033a2c
MartinAvagyan <martin.avagyan-cic.netherlands@ibm.com>
2018-02-01 15:43:47 +0100

    adding bluemix things

--- client_src/parking-app/src/app/app.routes.ts
+++ client_src/parking-app/src/app/app.routes.ts
@@ -2,14 +2,17 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {UserCardComponent} from "./main-content/user-card/user-card.component";
 import {SearchComponent} from "./main-content/search/search.component";
+import {AddPlateNumberComponent} from "./main-content/add-plate-number/add-plate-number.component";
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
   //{ path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: SearchComponent},
 
   { path: 'user/:plateNumber', component: UserCardComponent },
+  { path: 'add-user', component: AddPlateNumberComponent },
+
   // { path: 'home', component: HomeComponent },
   // { path: '**', component: ErrorComponent }
 ];
